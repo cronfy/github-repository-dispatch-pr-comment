e76983e4da84a8c65c3b198813069ee0a6ce193c
petrachev <petrachev@talon.one>
2025-04-08 12:03:31 +0200

    add buildkite download script

---
+++ .github/buildkite-download-coverage-artifacts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <build_number> <destination>"
+  exit 1
+fi
+
+if [ -z "$BUILDKITE_API_TOKEN" ] ; then
+    echo "Env var BUILDKITE_API_TOKEN is not set"
+    exit 1
+fi
+
+BUILD_NUMBER=$1
+ORG_SLUG="talon-dot-one"
+PIPELINE_SLUG="talon-service"
+BUILDKITE_API_URL="https://api.buildkite.com/v2/organizations/$ORG_SLUG/pipelines/$PIPELINE_SLUG/builds/$BUILD_NUMBER/artifacts"
+DOWNLOAD_DIR="./artifacts"
+
+mkdir -p "$DOWNLOAD_DIR"
+
+function requestGetArtifacts() {
+	curl -s -H "Authorization: Bearer $BUILDKITE_API_TOKEN" "$BUILDKITE_API_URL"
+}
+
+# Liists artifacts in 2 columns: download_url and filename
+function listArtifacts() {
+    requestGetArtifacts | jq -r '.[] | select(.filename | test("\\.coverage\\.out$")) | "\(.download_url) \(.filename)" '
+}
+
+listArtifacts | while read ARTIFACT_URL FILENAME; do
+    echo "Downloading artifact from: $ARTIFACT_URL"
+    curl -L -J -H "Authorization: Bearer $BUILDKITE_API_TOKEN" "$ARTIFACT_URL" -o "$DOWNLOAD_DIR/$FILENAME"
+done
+
